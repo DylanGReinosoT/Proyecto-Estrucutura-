448e62c083c1c73aeb015351b2fd273faafcf857
MyckelChamorro <mschamorro@espe.edu.ec>
2022-07-22 04:38:34 -0500

    borrado

--- Proyecto Arbol B/MenuOption.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include "MenuOption.h"
-
-MenuOption::MenuOption(const std::string title, Menu_Processing_Function_Ptr fuction)
-	: title_(title), p_processing_function_(fuction)
-{
-
-}
--- Proyecto Arbol B/MenuOptionM.cpp
+++
@@ -1,7 +0,0 @@
-#include "MenuOptionM.h"
-
-MenuOptionM::MenuOptionM(unsigned int number, const std::string title, Menu_Processing_Function_PtrM fuction)
-	: number_option_(number), title_(title), p_processing_function_(fuction)
-{
-
-}
